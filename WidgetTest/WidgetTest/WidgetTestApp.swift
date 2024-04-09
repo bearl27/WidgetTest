d43d179ee521ea56ddcc889e86e6929e95ff3ab6
bearl <125728897+bearl27@users.noreply.github.com>
2024-04-09 11:19:44 +0900

    First

---
+++ WidgetTest/WidgetTest/WidgetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTestApp.swift
+//  WidgetTest
+//
+//  Created by kai on 2024/04/09.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
